3d3f9ab2d17373d5a68419344ae05d9886863d30
Akash <akadhikary199901@gmail.com>
2024-01-30 01:51:09 +0600

    summation

--- AdvanceJava/sum.java
+++ AdvanceJava/sum.java
@@ -1,7 +1,14 @@
 package AdvanceJava;
-
+import java.util.Scanner;
 public class sum {
     public static void main(String[] args) {
-        System.out.println("Hello Worl!");
+        Scanner input= new Scanner(System.in);
+        int a, b, c;
+        System.out.println("Enter the value of a: ");
+        a = input.nextInt(); 
+        System.out.println("Enter the value of b: ");
+        b = input.nextInt();
+        c= a+b;
+        System.out.println("The summation c: " +c);
     }
 }
