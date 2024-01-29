69db680ab97e641a469d13d6e89133d632de46c1
Akash <akadhikary199901@gmail.com>
2024-01-30 02:37:21 +0600

    checking datatype and variable

---
+++ AdvanceJava/VariablesDatatypes.java
@@ -0,0 +1,18 @@
+package AdvanceJava;
+public class VariablesDatatypes{
+
+    public static void main(String[] args) {
+        int id, price;
+        String  tittle, description, category;
+        id = 101;
+        System.out.println("Id number: "+id);
+        tittle = "Iphone15";
+        System.out.println("Tittle: "+tittle);
+        price = 115000;
+        System.out.println("Price: "+price);
+        description = "Perfect product with image quality.";
+        System.out.println("Product overview: "+description);
+        category = "Phone";
+        System.out.println("Product Category: "+category);
+    }
+}
