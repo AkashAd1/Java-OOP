1056c00822004bc50dd2d5d8c5775d9cd2c11d7d
Akash <akadhikary199901@gmail.com>
2024-01-30 14:28:14 +0600

    Arithmetic operation checking

--- AdvanceJava/Product.java
+++ AdvanceJava/Product.java
@@ -1,33 +1,31 @@
 package AdvanceJava;
 import java.util.Scanner;
+
 public class Product {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Scanner ak = new Scanner(System.in);
         int id, price;
         String tittle, description, category;
 
         System.out.println("Product id: ");
         id = input.nextInt();
-
-        System.out.println("Enter product tittle: ");
-        tittle = ak.nextLine();
+        input.nextLine();
+        System.out.println("Enter product title: ");
+        tittle = input.nextLine();
 
         System.out.println("Product type: ");
-        category = ak.nextLine();
+        category = input.nextLine();
 
         System.out.println("Product overview: ");
-        description = ak.nextLine();
+        description = input.nextLine();
 
         System.out.println("Enter the price: ");
         price = input.nextInt();
 
-        System.out.println("Product ID: "+id);
-        System.out.println("Product Tittle: "+tittle);
-        System.out.println("Category: "+category);
-        System.out.println("Overview: "+description);
-        System.out.println("Price: "+price);
-
+        System.out.println("Product ID: " + id);
+        System.out.println("Product Title: " + tittle);
+        System.out.println("Category: " + category);
+        System.out.println("Overview: " + description);
+        System.out.println("Price: " + price);
     }
-
 }
---
+++ ArithmeticOperations/ArthOperation.java
@@ -0,0 +1,35 @@
+package ArithmeticOperations;
+import java.util.Scanner;
+public class ArthOperation {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1, num2, result;
+        float result2;
+
+        System.out.print("Enter the num1: ");
+        num1 = input.nextInt();
+        System.out.print("Enter the num2: ");
+        num2 = input.nextInt();
+
+        result = num1 + num2;
+        System.out.println("Addition: num1 + num2 = "+result);
+
+        result = num1 - num2;
+        System.out.println("Subtraction: num1 - num2 = "+result);
+
+        result = num1 * num2;
+        System.out.println("Multiplication: num1 * num2 = "+result);
+
+        result2 = (float)num1 / num2;
+        System.out.println("Divisor: num1 / num2 = "+result2);
+
+        result = num1 % num2;
+        System.out.println("Remainder: num1 % num2 = "+result);
+
+
+        
+        
+    }
+    
+}
