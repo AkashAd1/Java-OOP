ff78df90b3d8e338f36186b0996597194d63e0c4
Akash <akadhikary199901@gmail.com>
2024-02-01 00:53:20 +0600

    mathClass Checking.

---
+++ AdvanceJava/MathClass.java
@@ -0,0 +1,15 @@
+package AdvanceJava;
+
+public class MathClass {
+    public static void main(String[] args) {
+        int x = 2, y = 3;
+        int max = Math.max(x, y);
+        System.out.println("Maximum= "+max);
+        int min = Math.min(x, y);
+        System.out.println("Minimum= "+min);
+
+        double power = Math.pow(x, y);
+        System.out.println("X to the power y= "+power);
+
+    } 
+}
