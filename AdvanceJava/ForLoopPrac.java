3a8957ee9c3691a7c0f3825ff730744248690dbe
Akash <akadhikary199901@gmail.com>
2024-02-01 23:51:59 +0600

    For loop checking

---
+++ AdvanceJava/ForLoopPrac.java
@@ -0,0 +1,14 @@
+package AdvanceJava;
+
+public class ForLoopPrac {
+    public static void main(String[] args) {
+        int i;
+        for(i=0; i<10; i++){
+            System.out.println("Java");
+        }
+
+        for(i=10; i>=1; i--){
+            System.out.println(i);
+        }
+    }
+}
