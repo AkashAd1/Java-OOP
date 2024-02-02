03eb08818782fb122c6e09fde2c09d4837477929
Akash <akadhikary199901@gmail.com>
2024-02-03 01:27:13 +0600

    Assignment 10

---
+++ AdvanceJava/doWhileCheck.java
@@ -0,0 +1,12 @@
+package AdvanceJava;
+
+public class doWhileCheck {
+    public static void main(String[] args) {
+        int i=10;
+        do{
+            System.out.println(i);;
+            i--;
+        }while(i>=1);
+    }
+    
+}
