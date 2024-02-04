a0d45616740a6d3e8c324550d71c2b5cc78d7678
Akash <akadhikary199901@gmail.com>
2024-02-05 00:02:34 +0600

    Assignment on summation of m to n

---
+++ Assignment/Assignment12.java
@@ -0,0 +1,22 @@
+package Assignment;
+import java.util.Scanner;
+public class Assignment12 {
+    public static void main(String[] args) {
+        Scanner input =  new Scanner(System.in);
+        int i, m, n, sum;
+        sum = 0;
+
+        System.out.print("Enter the value of m: ");
+        m = input.nextInt();
+
+        System.out.print("Enter the value of n: ");
+        n = input.nextInt();
+
+        for(i=m; i<=n; i++){
+            if (i%2!=0) {
+                sum = sum + i;
+                System.out.println("The summation of odd: "+sum);
+            }
+        }
+    }
+}
