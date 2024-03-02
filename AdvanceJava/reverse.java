19793cda8cb5fe452ddf33763dcabe5b2cf36302
Akash <akadhikary199901@gmail.com>
2024-03-03 00:04:26 +0600

    sum of digit

---
+++ AdvanceJava/reverse.java
@@ -0,0 +1,21 @@
+package AdvanceJava;
+
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int temp, num;
+        System.out.print("Enter the number: ");
+        num = input.nextInt();
+
+        int sum = 0;
+        temp = num;
+        while(temp!=0){
+            int r = temp%10;
+            sum = sum + r;
+            temp = temp / 10;
+        }
+        System.out.println("The summation: "+sum);
+    }
+}
